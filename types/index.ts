ea36bfd84582a5cb6d5bf427cea3bc597fd4d4cd
ana-maria.popescu@isa.utm.md <ana-maria.popescu@isa.utm.md>
2025-09-05 02:45:33 +0300

    Rapid API done

--- types/index.ts
+++ types/index.ts
@@ -6,9 +6,27 @@ export interface CustomButtonProps {
     handleClick?:
     MouseEventHandler<HTMLButtonElement>;
     btnType?: "button" | "submit";
+    textStyles?:string;
+    rightIcon?:string;
+    isDisabled?:boolean;
 }
 
 export interface SearchManufacturerProps {
     manufacturer: string;
     setManufacturer: (manufacturer:string) => void;
+}
+
+export interface CarProps {
+    city_mpg:number; // 23 "this field is for premium subscribers only" ( bcs of that I used const as example )
+    class:string;
+    combination_mpg:number; //24 "this field is for premium subscribers only" ( bcs of that I used const as example )
+    cylinders:number;
+    displacement:number;
+    drive:string;
+    fuel_type:string;
+    highway_mpg:number; // 26 "this field is for premium subscribers only"; ( bcs of that I used const as example )
+    make:string;
+    model:string;
+    transmission:string;
+    year:number;
 }
