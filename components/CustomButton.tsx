dc80b7698a80a3634cb8613d4b339c692389e8b0
ana-maria.popescu@isa.utm.md <ana-maria.popescu@isa.utm.md>
2025-09-04 22:41:01 +0300

    fixed tailwind

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -8,7 +8,7 @@ const CustomButton = ( { title, containerStyles, handleClick }: CustomButtonProp
         <button
             disabled={false}
             type={"button"}
-            className={'custom-btn ${containerStyles}'}
+            className={`custom-btn ${containerStyles}`}
             onClick={handleClick}
         >
             <span className={'flex-1'}>
